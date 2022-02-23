2ed27779a0c5a03c51008d2ad27d06377997b70b
Henry Miller <mail.henrym+git@gmail.com>
2022-02-23 11:09:05 -0500

    commit tree 055e2aa136936ad599ffc63b14a9377ea3cb41a8

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,28 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+   self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    count = 0
+    self.split(' ').each do |word| 
+      if word.sentence?
+        count+=1
+      elsif word.question?
+        count+=1
+      elsif word.exclamation?
+        count+=1
+      end
+    end
+    count
   end
 end
